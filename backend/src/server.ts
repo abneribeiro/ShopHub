965efba8cd646749a4e7ed920a40aec5dc71a168
abneribeiroo <eliasabner38@gmail.com>
2025-01-14 21:40:40 +0000

    feat: add multer for file upload handling and configure static file serving

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
+import path from "path";
 import { connectDB } from "./config/database";
-// import authRoutes from './routes/authRoutes';
 import productRoutes from "./routes/productRoutes";
-
+// import authRoutes from "./routes/authRoutes";
 dotenv.config();
 
 const app = express();
@@ -13,12 +14,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// File upload configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
 // Routes
 // app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes)
+app.use('/api/products', upload.single('image'), productRoutes);
+
+// Serve uploaded files
+app.use('/uploads', express.static('uploads'));
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from the backend! Now is working" });
+  res.json({ message: "Hello from the backend!" });
 });
 
 const PORT = process.env.PORT || 3000;
