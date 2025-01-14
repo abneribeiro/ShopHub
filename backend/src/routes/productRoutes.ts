fe14986b24fb2e1ec5efbc4973e39ca1d3f1c020
abneribeiroo <eliasabner38@gmail.com>
2025-01-14 21:40:49 +0000

    feat: add deleteProduct route to enable product deletion in productRoutes

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct } from '../controllers/productController';
+import { getProducts, getProductById, createProduct, deleteProduct } from '../controllers/productController';
 import authMiddleware from '../middleware/auth';
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
+router.delete('/:id',deleteProduct);
 router.post('/', authMiddleware, createProduct);
 
 export default router;
